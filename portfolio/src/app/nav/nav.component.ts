84769663db4d33ed9a83f1d3567ae92ffd6e631b
pablorjd1 <pablo.jimenez2221@gmail.com>
2024-08-16 23:35:26 -0400

    update

---
+++ portfolio/src/app/nav/nav.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from "../home/home.component";
+import { ProjectComponent } from '../project/project.component';
+import { AboutComponent } from "../about/about.component";
+
+
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+    AsyncPipe,
+    HomeComponent,
+    ProjectComponent,
+    AboutComponent
+]
+})
+export class NavComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
